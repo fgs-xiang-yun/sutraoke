dc1d75580c78486b79d5abe7012e5124c8d6013c
Summer Seo <summeryseo@gmail.com>
2023-03-16 09:37:46 -0500

    Initial commit

---
+++ src/Sutra.ts
@@ -0,0 +1,13 @@
+export const Sutra = {
+  "Heart Sutra": `Bodhisattva Avalokitesvara, while practicing the profound Prajna Paramita, perceives that all Five Skandhas are empty, thereby overcoming all suffering.
+Sariputra, form does not differ from emptiness, emptiness does not differ from form, form is just emptiness, emptiness is just form. The same is true of sensations, perceptions, desires and consciousness.
+Sariputra, this is the emptiness of all dharmas: they neither arise nor cease, are neither defiled nor pure, do not increase or decrease. Therefore, within emptiness there is no form, no sensation, perception, volition, consciousness;
+no eyes, no ears, no nose, no tongue, no body, no mind; no sight, no sound, no scent, no taste, no touch, no object of mind; no realm of eyes... even no thinking; no ignorance nor end of ignorance... even no aging and death, nor end of aging and death; no suffering, no origin, no cessation, no path; no wisdom and nothing to attain.
+Because nothing is attained, bodhisattvas maintain Prajna Paramita, then their hearts are without hindrance, and without hindrance, without fear; escaping upside-down, dream-like thinking, perfectly realizing nirvana.
+All Buddha's of all times maintain Prajna Paramita, thus attaining Anuttara Samyak Sambodhi. Hence know, Prajna Paramita is the all-powerful mantra, the great enlightening mantra, the unexcelled matra, the unequaled mantra, able to dispel all suffering. This is true, not false. Therefore proclaim the Prajna Paramita mantra. Recite the mantra thus:
+Gate gate paragate parasamgate bodhi svaha!
+Gate gate paragate parasamgate bodhi svaha!
+Gate gate paragate parasamgate bodhi svaha!`,
+} as const;
+
+export type Sutra = keyof typeof Sutra;
