b7a514fcdd3d6e0a9b9b7094b7035212561540b3
Tam Nguyen <tamsanh@gmail.com>
2023-03-17 17:46:41 -0500

    Allow space as key

--- src/App.tsx
+++ src/App.tsx
@@ -225,7 +225,7 @@ function App() {
   useEffect(() => {
     const handler = (event: KeyboardEvent) => {
       // Key List: https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values#navigation_keys
-      const nextKeys = ["ArrowDown", "ArrowRight"];
+      const nextKeys = ["ArrowDown", " ", "ArrowRight"];
       const prevKeys = ["ArrowUp", "ArrowLeft"];
 
       if (nextKeys.includes(event.key)) {
