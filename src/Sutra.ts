c1ba0e5e41f31709ced61ce8ee91b78edde0de3c
Tam Nguyen <tamsanh@gmail.com>
2023-03-17 18:08:44 -0500

    Tweak sutra

--- src/Sutra.ts
+++ src/Sutra.ts
@@ -5,7 +5,7 @@ A*va*lo*ki*tes*va*ra, while prac*ti*cing the pro*found Praj*na Pa*ra*mi*ta, per*
 Five Skan*dhas are emp*ty, there*by o*ver*co*ming all suf*fer*ing.
 Sa*ri*pu*tra, form does not dif*fer from emp*ti*ness, emp*ti*ness does not dif*fer from form,
 form is just emp*ti*ness, emp*ti*ness is just form. The same is true of sen*sa*tions, per*cep*tions, de*si*res and con*scious*ness.
-Sa*ri*pu*tra, this is the emp*ti*ness of all dhar*mas: they nei*ther a*rise nor cease, are nei*ther de*fil*ed nor pure,
+Sa*ri*pu*tra, this is the emp*ti*ness of all dhar*mas: they nei*ther a*rise nor cease, are nei*ther de*filed nor pure,
 do not in*crease or de*crease. There*fore, with*in emp*ti*ness there is no form, no sen*sa*tion, per*cep*tion, vo*li*tion, con*scious*ness;
 no eyes, no ears, no nose, no tongue, no bo*dy, no mind; no sight, no sound, no scent, no taste,
 no touch, no ob*ject of mind; no realm of eyes... e*ven no think*ing; no ig*nor*ance nor end of ig*nor*ance... e*ven no a*ging and death,
